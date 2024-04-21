fdc7f69ccbcafd3b7d69eb0946ab9377b42ad27a
Muhammed Said Uraz <msaiduraz@gmail.com>
2024-04-21 03:07:20 +0300

    Collectable rotation implemented

--- Assets/Scripts/CollectableModule/BaseCollectable.cs
+++ Assets/Scripts/CollectableModule/BaseCollectable.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 using Assets.Scripts.Interfaces.Interaction;
 
 namespace Assets.Scripts.Collectables
@@ -7,24 +8,42 @@ namespace Assets.Scripts.Collectables
     {
         #region Variables
 
+        private Tween _rotationTween;
+
+        [Header("Components")]
+        [SerializeField] private float _rotationDuration = 2.0f;
+
         [Header("Components")]
         [SerializeField] protected GameObject mesh;
         [SerializeField] protected Collider collectableCollider;
 
         #endregion Variables
 
-        #region Properties
+        #region Unity Functions
 
+        private void Awake()
+        {
+            PlayRotationAnimation();
+        }
 
-
-        #endregion Properties
+        #endregion Unity Functions
 
         #region Functions
 
         protected void Disable()
         {
+            _rotationTween?.Kill();
+
             collectableCollider.enabled = false;
             gameObject.SetActive(false);
+        }
+
+        private void PlayRotationAnimation()
+        {
+            _rotationTween = transform.DORotate(360.0f * Vector3.up, _rotationDuration)
+                .SetEase(Ease.Linear)
+                .SetRelative()
+                .SetLoops(-1);
         }
 
         public abstract void OnInteract();
